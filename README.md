dfaad7bfa02e499624b31e8d0ffa81fbcc268c4c
joseluispr13 <142362140+joseluispr13@users.noreply.github.com>
2024-09-03 17:10:21 -0600

    Tarea 2

---
+++ README.md
@@ -0,0 +1,2 @@
+# Programacion_Web_2_1710
+Repositorio correspondiente a la materia de Programación Web 2 del grupo 1710 a cargo del profesor Aaron Velasco Agustín
---
+++ app.js
@@ -0,0 +1,21 @@
+
+alert("Este breve formulario permitirá conocer algunos gustos sobre del usuario");
+
+let deporte = prompt("Ingresa tu deporte favorito: ");
+alert("Tu deporte favorito es: " + deporte);
+console.log("Resumen de datos");
+console.log("Tu deporte favorito es: " + deporte);
+
+let comida = prompt("Ingresa tu comida favorita: ");
+alert("Tu comida favorita es: " + comida);
+console.log("Tu comida favorita es: " + comida);
+
+let serie = prompt("Ingresa tu serie o película favorita: ");
+alert("Tu serie o película favorita es: " + serie);
+console.log("Tu serie o película favorita es: " + serie);
+
+alert("Hasta luego, vuelva pronto :)");
+
+
+
+
